a04f899bd4a2f8fb4ed3ac0da348ea4f052c9c1f
ErikaLFrost <erika.loman@gmail.com>
2019-04-09 14:06:03 +0200

    Validering

--- validate.js
+++ validate.js
@@ -1,5 +1,6 @@
 (function() {
 
+    // Validering för alla required-fält (Validerar att det är fler än 2 tecken och att de inte består enbart av siffror)
     $("input[required]").keyup(function() {
         if ($(this).val().length <= 2 || $.isNumeric($(this).val())){
             $(this).siblings(".invalid-feedback").show();
@@ -8,5 +9,15 @@
             $(this).siblings(".invalid-feedback").hide();
     }
     });
+
+    //Validering för e-mail (validerar att @ finns med i inputen)
+    $("#email").keyup(function(){
+        if ($(this).val().indexOf("@") > -1){
+            $(this).siblings(".invalid-feedback").hide();
+        }
+        else{
+            $(this).siblings(".invalid-feedback").show();
+        }
+    })
     
   })();
